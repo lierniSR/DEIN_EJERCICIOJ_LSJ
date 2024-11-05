d31f075a7dcf5a9aa2b41eda8ee79ebe895814bd
lierniSR <lierniSR@github.com>
2024-11-05 18:17:26 +0100

    La interfaz se ve bien

--- src/main/java/es/liernisarraoa/dein_ejercicioj_lsj/Cooper.java
+++ src/main/java/es/liernisarraoa/dein_ejercicioj_lsj/Cooper.java
@@ -3,6 +3,7 @@ package es.liernisarraoa.dein_ejercicioj_lsj;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,10 @@ import java.io.IOException;
 public class Cooper extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Cooper.class.getResource("cooper.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+        FXMLLoader fxmlLoader = new FXMLLoader(Cooper.class.getResource("cooperLayout.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Cooper!");
+        stage.getIcons().add(new Image(String.valueOf(Cooper.class.getResource("Imagenes/Cooper.png"))));
         stage.setScene(scene);
         stage.show();
     }
